dfac2c4d9303ceda466c4edb8e104fcf9b3b54a2
Anupam <anupam@Anupams-MacBook-Air.local>
2019-12-24 23:06:33 +0530

    abhishek/change for pull

--- support_system/complaint/models.py
+++ support_system/complaint/models.py
@@ -2,9 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-# Create your models here.
-
-
 class Complaint(models.Model):
     user = models.ForeignKey(User, models.CASCADE)
     title = models.CharField(max_length=50)
